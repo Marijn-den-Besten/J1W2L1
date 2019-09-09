8889e7c1a30eb0589ecd5567328989facef685af
Marijn <marijndbesten@gmail.com>
2019-09-09 13:33:07 +0200

    JS toevoegen

---
+++ b1w2l1.js
@@ -0,0 +1,20 @@
+/*
+Marijn den Besten
+ICT Applicatie ontwikkeling
+*/
+
+alert('Je krijgt nu drie vragen') //Doet een popup voor de vragen
+
+var naam = prompt('Voer je naam in') //Maakt een naam variable aan
+
+var achternaam = prompt('Voer je achternaam in') //Maakt een achternaam variable aan
+
+var leeftijd = prompt('vul je leeftijd in') //Maakt een leeftijd variable aan
+
+document.write(naam) //Schrijft naam op
+document.write ("<br />") //Doet een enter
+
+document.write (achternaam) //Schrijft achternaam op
+document.write ("<br />") //Doet een enter
+
+document.write (leeftijd) //Schrijft leeftijd op
