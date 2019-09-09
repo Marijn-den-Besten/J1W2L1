bc5949b61616bc0688e70f317325cff89a5754d3
Marijn <marijndbesten@gmail.com>
2019-09-09 13:36:01 +0200

    Js toevoegen met commentaar

--- b1w2l1.js
+++ b1w2l1.js
@@ -5,11 +5,11 @@ ICT Applicatie ontwikkeling
 
 alert('Je krijgt nu drie vragen') //Doet een popup voor de vragen
 
-var naam = prompt('Voer je naam in') //Maakt een naam variable aan
+var naam = prompt('Voer je naam in') //Maakt een naam variable aan en maakt een popup
 
-var achternaam = prompt('Voer je achternaam in') //Maakt een achternaam variable aan
+var achternaam = prompt('Voer je achternaam in') //Maakt een achternaam variable aan en maakt een popup
 
-var leeftijd = prompt('vul je leeftijd in') //Maakt een leeftijd variable aan
+var leeftijd = prompt('vul je leeftijd in') //Maakt een leeftijd variable aan en maakt een popup
 
 document.write(naam) //Schrijft naam op
 document.write ("<br />") //Doet een enter
