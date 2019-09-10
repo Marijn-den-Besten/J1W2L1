6d57bf7243b5aa6c002ca45d79a753dbb6479118
Marijn <marijndbesten@gmail.com>
2019-09-10 14:38:23 +0200

    BOeie

--- b1w2lPizza.js
+++ b1w2lPizza.js
@@ -1,11 +1,23 @@
-function Pizza(){
+/*function Pizza(){
 	var a = parseInt(document.querySelector("#small").value);
 	var b = parseInt(document.querySelector("#medium").value);
 	var c = parseInt(document.querySelector("#large").value);
 
-	var d = parseInt(a * 5.25);
-	var e = parseInt(b * 5.99);
-	var f = parseInt(c * 10.99);
+	var d = parseInt(a * 5);
+	var e = parseInt(b * 8);
+	var f = parseInt(c * 12);
 
-	alert("€" + d + " " + "€" + e + " " + "€" + f);
-}
+	document.write("€" + d + " " + "€" + e + " " + "€" + f);
+}	*/
+
+var a = parseInt(prompt("Hoeveel small pizzas wil je?"));
+var b = parseInt(prompt("Hoeveel medium pizzas wil je?"));
+var c = parseInt(prompt("Hoeveel large pizzas wil je?"));
+
+var d = parseInt(a * 5);
+var e = parseInt(b * 8);
+var f = parseInt(c * 12);
+
+document.write("€" + d + " " + "€" + e + " " + "€" + f);	
+
+
