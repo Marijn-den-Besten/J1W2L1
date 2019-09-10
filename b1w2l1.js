efe2eede4de33133444df9528460aa89ccec858e
Marijn <marijndbesten@gmail.com>
2019-09-10 10:06:58 +0200

    Gebeure

--- b1w2l1.js
+++ b1w2l1.js
@@ -7,14 +7,19 @@ alert('Je krijgt nu drie vragen') //Doet een popup voor de vragen
 
 var naam = prompt('Voer je naam in') //Maakt een naam variable aan en maakt een popup
 
-var achternaam = prompt('Voer je achternaam in') //Maakt een achternaam variable aan en maakt een popup
-
-var leeftijd = prompt('vul je leeftijd in') //Maakt een leeftijd variable aan en maakt een popup	
-
 document.write(naam) //Schrijft naam op
 document.write ("<br />") //Doet een enter
+
+var achternaam = prompt('Voer je achternaam in') //Maakt een achternaam variable aan en maakt een popup
 
 document.write (achternaam) //Schrijft achternaam op
 document.write ("<br />") //Doet een enter
 
+var leeftijd = prompt('vul je leeftijd in') //Maakt een leeftijd variable aan en maakt een popup				
+
 document.write (leeftijd) //Schrijft leeftijd op
+document.write("<br />") //Doet een enter
+
+var a = parseInt(document.querySelector("#Value1").Value1)
+
+document.write(a)
---
+++ b1w2l3.js
@@ -0,0 +1,6 @@
+function calc(){
+var a = parseInt(document.querySelector("#value1").value);
+var Calculate;
+}
+document.write(a);
+
