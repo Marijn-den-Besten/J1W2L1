d384ef9f38b214b0ca3fad844b4e2f528aadc395
Marijn <marijndbesten@gmail.com>
2019-09-11 09:26:48 +0200

    Goeie shit

--- b1w2lPizza.js
+++ b1w2lPizza.js
@@ -10,14 +10,24 @@
 	document.write("€" + d + " " + "€" + e + " " + "€" + f);
 }	*/
 
-var a = parseInt(prompt("Hoeveel small pizzas wil je?"));
-var b = parseInt(prompt("Hoeveel medium pizzas wil je?"));
-var c = parseInt(prompt("Hoeveel large pizzas wil je?"));
+var a = parseInt(prompt("Hoeveel small pizzas wil je? : €5,00"));
+var b = parseInt(prompt("Hoeveel medium pizzas wil je? : €8,00"));
+var c = parseInt(prompt("Hoeveel large pizzas wil je? : €12,00"));
 
 var d = parseInt(a * 5);
 var e = parseInt(b * 8);
 var f = parseInt(c * 12);
 
-document.write("€" + d + " " + "€" + e + " " + "€" + f);	
+document.write(a + " x " + "Small = " + "€" + d);
+document.write("<br />");
+
+document.write(b + " x " + "Medium = " + "€" + e);
+document.write("<br />");
+
+document.write(c + " x " + "Large = " + "€" + f);
+document.write("<br />");
+document.write("<br />");
+
+document.write("Totaal: " + "€" + (d + e + f));
 
 
