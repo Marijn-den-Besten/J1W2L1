300aceebb2ff3d9b0a9aae3b92fb7fe6a655cd7c
Marijn <marijndbesten@gmail.com>
2019-09-11 09:38:27 +0200

    KLAAR!!

--- b1w2lPizza.js
+++ b1w2lPizza.js
@@ -1,14 +1,3 @@
-/*function Pizza(){
-	var a = parseInt(document.querySelector("#small").value);
-	var b = parseInt(document.querySelector("#medium").value);
-	var c = parseInt(document.querySelector("#large").value);
-
-	var d = parseInt(a * 5);
-	var e = parseInt(b * 8);
-	var f = parseInt(c * 12);
-
-	document.write("€" + d + " " + "€" + e + " " + "€" + f);
-}	*/
 
 var a = parseInt(prompt("Hoeveel small pizzas wil je? : €5,00"));
 var b = parseInt(prompt("Hoeveel medium pizzas wil je? : €8,00"));
